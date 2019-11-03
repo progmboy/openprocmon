0a30cb700bdad252667eebdf864691c272fb32ab
progmboy <progmboy@gmail.com>
2019-11-03 21:42:16 +0800

    clean source

--- gui/main.cpp
+++ gui/main.cpp
@@ -14,7 +14,7 @@
 #include "propstack.h"
 #include "filter.hpp"
 #include "fltprocess.h"
-#include "filerdlg.h"
+#include "filterdlg.h"
 #include "MainFrm.h"
 
 #pragma comment(lib, "Shcore.lib")
@@ -44,11 +44,6 @@ int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
 {
-#if 1
-	int CxLarge = GetSystemMetrics(SM_CXICON);
-	int CxSmall = GetSystemMetrics(SM_CXSMICON);
-	int CyLarge = GetSystemMetrics(SM_CYICON);
-	int CySmall = GetSystemMetrics(SM_CYSMICON);
 	HRESULT hRes = ::CoInitialize(NULL);
 	ATLASSERT(SUCCEEDED(hRes));
 
@@ -64,5 +59,4 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lp
 	::CoUninitialize();
 
 	return nRet;
-#endif
 }
